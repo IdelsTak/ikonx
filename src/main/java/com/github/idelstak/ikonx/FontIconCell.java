e2e88d5a28be39be7aac139c0bbabe40957a4b68
Hiram K <hiram.kamau@outlook.com>
2023-09-07 05:24:38 +0300

    Update FontIconCell.java

--- src/main/java/com/github/idelstak/ikonx/FontIconCell.java
+++ src/main/java/com/github/idelstak/ikonx/FontIconCell.java
@@ -1,8 +1,8 @@
 package com.github.idelstak.ikonx;
 
-import javafx.scene.control.ContentDisplay;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableCell;
+import javafx.scene.control.*;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.util.List;
@@ -21,6 +21,12 @@ class FontIconCell extends TableCell<List<PackIkon>, PackIkon> {
         root.getStyleClass().addAll("icon-label", "text-small");
 
         fontIcon.iconColorProperty().bind(root.textFillProperty());
+
+        root.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 1) {
+                copyIconCodeToClipboard();
+            }
+        });
     }
 
     @Override
@@ -32,8 +38,24 @@ class FontIconCell extends TableCell<List<PackIkon>, PackIkon> {
             return;
         }
 
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem copyItem = new MenuItem("Copy icon code");
+        copyItem.setOnAction(event -> copyIconCodeToClipboard());
+        contextMenu.getItems().add(copyItem);
+        root.setContextMenu(contextMenu);
+
         root.setText(packIkon.ikon().getDescription());
         fontIcon.setIconCode(packIkon.ikon());
         setGraphic(root);
+    }
+
+    private void copyIconCodeToClipboard() {
+        String iconCode = fontIcon.getIconCode().getDescription();
+
+        if (iconCode != null && !iconCode.isEmpty()) {
+            ClipboardContent content = new ClipboardContent();
+            content.putString(iconCode);
+            Clipboard.getSystemClipboard().setContent(content);
+        }
     }
 }

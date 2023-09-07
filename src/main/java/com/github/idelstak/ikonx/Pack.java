8033d48e78f1711d1aeb96a74e7ce50e069fd770
Hiram K <hiram.kamau@outlook.com>
2023-09-07 05:42:55 +0300

    Update Pack.java

--- src/main/java/com/github/idelstak/ikonx/Pack.java
+++ src/main/java/com/github/idelstak/ikonx/Pack.java
@@ -2,16 +2,29 @@ package com.github.idelstak.ikonx;
 
 import org.kordamp.ikonli.Ikon;
 import org.kordamp.ikonli.bootstrapicons.BootstrapIcons;
+import org.kordamp.ikonli.fontawesome.FontAwesome;
+import org.kordamp.ikonli.fontawesome5.FontAwesomeBrands;
+import org.kordamp.ikonli.fontawesome5.FontAwesomeRegular;
+import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
 import org.kordamp.ikonli.material.Material;
 import org.kordamp.ikonli.materialdesign.MaterialDesign;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public enum Pack {
     BOOTSTRAP("Bootstrap", BootstrapIcons.values()),
     MATERIAL("Material", Material.values()),
-    MATERIAL_DESIGN("Material Design", MaterialDesign.values());
+    MATERIAL_DESIGN("Material Design", MaterialDesign.values()),
+    FONT_AWESOME("Font Awesome", FontAwesome.values()),
+    FONT_AWESOME_5("Font Awesome 5", Stream.concat(
+            Stream.concat(
+                    Arrays.stream(FontAwesomeBrands.values()).map(brands -> (Ikon) brands),
+                    Arrays.stream(FontAwesomeRegular.values()).map(regular -> (Ikon) regular)
+            ),
+            Arrays.stream(FontAwesomeSolid.values()).map(solid -> (Ikon) solid)
+    ).toArray(Ikon[]::new));
 
     private final String description;
     private final Ikon[] ikons;
@@ -21,7 +34,7 @@ public enum Pack {
         this.ikons = copy(ikons);
     }
 
-    public static Pack from(String text){
+    public static Pack from(String text) {
         return Arrays.stream(values())
                 .filter(pack -> Objects.equals(pack.description, text))
                 .findFirst()
